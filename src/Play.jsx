b8aa2e2f4f8a942aba025d8bff736763488ba758
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-09-03 20:26:36 +0500

    add loader

--- src/Play.jsx
+++ src/Play.jsx
@@ -1,16 +1,30 @@
 import HeadCon from './HeadCon';
 import BodyCon from './BodyCon';
-import { useState } from 'react';
-// import {PacmanLoader} from 'react-spinners'
-{/* <PacmanLoader color='red' /> */}
+import { useEffect, useState } from 'react';
+import {PacmanLoader} from 'react-spinners'
 function Play() {
   let [num,SetNum] = useState(0);
   let [roll,setRoll] = useState(1);
   let [result,setResult] = useState(0);
+  let [loader,SetLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      SetLoader(false);
+    },3000)
+  })
   return (
     <>
+    {
+      loader ? 
+      <div className='h-screen flex justify-center items-center'>
+        <PacmanLoader color='red' />
+      </div>
+      :
+      <>
    <HeadCon num={num} SetNum={SetNum} result={result}></HeadCon>
    <BodyCon roll={roll} setRoll={setRoll} result={result} setResult={setResult} num={num}></BodyCon>
+      </>
+    }
     </>
   )
 }
